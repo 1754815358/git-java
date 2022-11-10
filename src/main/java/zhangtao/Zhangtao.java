503e4abccaf0f824c64344e40f4bc7737d55409f
1754815358 <110961572+1754815358@users.noreply.github.com>
2022-11-10 22:46:08 +0800

    Update Zhangtao.java
    额呵呵呵

--- src/main/java/zhangtao/Zhangtao.java
+++ src/main/java/zhangtao/Zhangtao.java
@@ -4,15 +4,6 @@ public class Zhangtao {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("hello2");
-        System.out.println("hello3");
-        System.out.println("紧急修复");
-        System.out.println("用户等待三分钟");
-        System.out.println("用户等待2分钟");
-        System.out.println("用户不想等了");
-        System.out.println("用户说：是的不想等了");
-        System.out.println("用户必须等");
-        System.out.println("看看用户说什么吧");
-        System.out.println("你们都不要说了");
-        System.out.println("我觉得。。。。。");
+   
     }
 }
